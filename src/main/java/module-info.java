2d2ad8b7eb80d655df572af970712becb81c9bac
Melvin Cabatuan <melvincabatuan@users.noreply.github.com>
2025-06-15 17:08:44 +0800

    Update module-info.java
    updated javafx.controls

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,6 @@ module ph.edu.dlsu.lbycpei.loginexample {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-
     opens ph.edu.dlsu.lbycpei.loginexample to javafx.fxml;
     exports ph.edu.dlsu.lbycpei.loginexample;
 }
